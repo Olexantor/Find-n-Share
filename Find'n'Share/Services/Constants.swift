220159545f3564b7e01101d1ab2cba19056aa1f0
Olexantor <nikolaev.warrior@gmail.com>
2022-02-17 16:52:02 +0500

    A model for data parsing was set up, also network manager was set up

---
+++ Find'n'Share/Services/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Find'n'Share
+//
+//  Created by Александр on 17.02.2022.
+//
+
+import Foundation
+
+let urlSerpapi = "https://serpapi.com/search.json?q="
+let serpapiKey = "&tbm=isch&ijn=0&api_key=e95d89359f42627ff1ec1a8b9289cd56bdf683c6d588792867e6e905d864cba1"
