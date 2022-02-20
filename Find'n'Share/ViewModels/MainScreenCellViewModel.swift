ba0e38f487c9c809f58df2e7e66fda92105a7dbc
Olexantor <nikolaev.warrior@gmail.com>
2022-02-20 20:11:55 +0500

    setting the cell model and displaying content in the cell

---
+++ Find'n'Share/ViewModels/MainScreenCellViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  MainScreenCellViewModel.swift
+//  Find'n'Share
+//
+//  Created by Александр on 20.02.2022.
+//
+import Foundation
+
+class MainScreenCellViewModel: MainScreenCellViewModelType {
+    var urlOfPicture: URL
+
+    init(urlOfPicture: URL) {
+        self.urlOfPicture = urlOfPicture
+    }
+}
